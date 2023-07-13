18427590849eb9ea8df7490b824c8a08bdcc58bf
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-07-13 10:20:28 -0500

    updates to final files

--- explore.py
+++ explore.py
@@ -21,3 +21,23 @@ def get_parents_graph():
     plt.xticks([0,1], ['No College Degree', 'Has College Degree'], )
     plt.legend(labels)
     plt.show()
+    
+def get_parents_stats():
+    df = pd.read_csv('preprocessed_edu_data.csv')
+    drop_list = ['is_male', 'completed_test_prep', 'practiced_sport', 'wkly_study_hours_< 5', 'wkly_study_hours_> 10']
+    df.drop(columns= drop_list, inplace= True)
+    train, validate, test = w.split(df)
+    college_degree_crosstab = pd.crosstab(train.has_college_degree, train.risk_cat)
+    chi2, p_value, dof, expected = chi2_contingency(college_degree_crosstab)
+
+    # Print the results
+    print("Chi-square statistic:", chi2)
+    print("p-value:", p_value)
+    print("Degrees of freedom:", dof)
+    print("Expected frequencies:", expected)
+    alpha = 0.05  # Significance level
+
+    if p_value < alpha:
+        print("Reject the null hypothesis")
+    else:
+        print("Fail to reject the null hypothesis")
